6f2c3ce8d5787bba224fbb157adb22257997660f
DESKTOP-IGL44SB\aaaaa <hmj2422036@stu.o-hara.ac.jp>
2025-08-27 01:27:10 +0900

    Hpバーを追加

---
+++ Assets/GameScript/ShowCastleHelth.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ShowCastleHelth : MonoBehaviour
+{
+    [SerializeField] Slider HelthGage;
+
+    void Update()
+    {
+          
+    }
+}
